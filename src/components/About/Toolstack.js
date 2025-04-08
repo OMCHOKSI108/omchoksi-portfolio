19bad682bffaa40518b3ca6d8f3f13df8073fad2
omchoksi108 <omchoksi108@gmail.com>
2025-04-08 17:43:20 +0530

    chore: replace SiMacos with SiGooglecolab and add SiKaggle icon

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,14 +5,15 @@ import {
   SiPostman,
   SiSlack,
   SiVercel,
-  SiMacos,
+  SiGooglecolab,
+  SiKaggle,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiGooglecolab />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
@@ -25,6 +26,9 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+      </Col>
+       <Col xs={4} md={2} className="tech-icons">
+        <SiKaggle />
       </Col>
     </Row>
   );
