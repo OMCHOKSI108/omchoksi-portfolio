072e8379dcb9c425e9b7548cd69e4c2ce1df1379
omchoksi108 <omchoksi108@gmail.com>
2025-04-08 17:47:35 +0530

    chore: reorder and update tech icons in Toolstack component

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,30 +5,58 @@ import {
   SiPostman,
   SiSlack,
   SiVercel,
+  SiGithub,
+  SiNetlify,
+  SiJupyter,
   SiGooglecolab,
+  SiStreamlit,
   SiKaggle,
+  SiXampp,
+  SiFigma,
+  SiBurpsuite
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiGooglecolab />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <SiJupyter />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglecolab />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiStreamlit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiKaggle />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
-       <Col xs={4} md={2} className="tech-icons">
-        <SiKaggle />
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiXampp />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBurpsuite />
       </Col>
     </Row>
   );
