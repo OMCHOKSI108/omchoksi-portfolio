f12decb94d400ba6844b842185ab6b6306236dd9
omchoksi108 <omchoksi108@gmail.com>
2025-04-08 21:16:17 +0530

    chore: simplify About component by removing unused icons and links

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,8 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
-import { FaGlobe, FaCode } from "react-icons/fa"; // Generic code icon for unsupported languages
-import { SiLeetcode, SiHackerrank, SiCodeforces, SiCodechef, SiKaggle, SiPython, SiJavascript, SiHtml5, SiCss3, SiNodedotjs, SiReact, SiMongodb, SiMysql, SiGit, SiLinux, SiDocker, SiFirebase, SiVisualstudiocode, SiGithub } from "react-icons/si";
+import { FaGlobe } from "react-icons/fa";
+import { SiLeetcode, SiHackerrank, SiCodeforces, SiCodechef, SiKaggle } from "react-icons/si";
 
 function About() {
   return (
@@ -94,147 +94,6 @@ function About() {
               <div className="profile-box">
                 <img src={require("../../Assets/gfg-logo.png")} alt="GFG" style={{ width: "40px", height: "40px" }} />
                 <p>GFG</p>
-              </div>
-            </a>
-          </Col>
-        </Row>
-        <h1 className="project-heading">
-          <FaGlobe /> <strong className="purple">Tech Documentation</strong>
-        </h1>
-        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://devdocs.io/c/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <img src={require("../../Assets/c-icon.png")} alt="C" style={{ width: "40px", height: "40px" }} />
-                <p>C</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://devdocs.io/cpp/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <img src={require("../../Assets/cpp-icon.png")} alt="C++" style={{ width: "40px", height: "40px" }} />
-                <p>C++</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://docs.oracle.com/javase/tutorial/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <img src={require("../../Assets/java-icon.png")} alt="Java" style={{ width: "40px", height: "40px" }} />
-                <p>Java</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://docs.python.org/3/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiPython size={40} color="white" />
-                <p>Python</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiJavascript size={40} color="white" />
-                <p>JS</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiHtml5 size={40} color="white" />
-                <p>HTML5</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiCss3 size={40} color="white" />
-                <p>CSS3</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://nodejs.org/docs/latest/api/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiNodedotjs size={40} color="white" />
-                <p>Node.js</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://reactjs.org/docs/getting-started.html" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiReact size={40} color="white" />
-                <p>React</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://docs.mongodb.com/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiMongodb size={40} color="white" />
-                <p>MongoDB</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://dev.mysql.com/doc/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiMysql size={40} color="white" />
-                <p>MySQL</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://git-scm.com/doc" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiGit size={40} color="white" />
-                <p>Git</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://www.linux.org/docs.php" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiLinux size={40} color="white" />
-                <p>Linux</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://docs.docker.com/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiDocker size={40} color="white" />
-                <p>Docker</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://firebase.google.com/docs" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiFirebase size={40} color="white" />
-                <p>Firebase</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://code.visualstudio.com/docs" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiVisualstudiocode size={40} color="white" />
-                <p>VS Code</p>
-              </div>
-            </a>
-          </Col>
-          <Col xs={4} md={2} className="profile-card">
-            <a href="https://docs.github.com/" target="_blank" rel="noopener noreferrer" className="profile-link">
-              <div className="profile-box">
-                <SiGithub size={40} color="white" />
-                <p>GitHub</p>
               </div>
             </a>
           </Col>
